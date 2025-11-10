9d0b7575dc989502ed11f910f42d96b17f17d0c8
Steve Wolfe <swolfe100@gmail.com>
2025-11-10 16:49:51 -0500

    first commit

--- create-repo.sh
+++ create-repo.sh
@@ -5,5 +5,7 @@ git status
 git commit -m "first commit"
 git branch -M main
 #git remote add origin https://github.com/steve100/Whisperx.git
+# just in case
+git remote remove origin
 git remote add origin git@github.com:steve100/whisperx.git
 git push -u origin main
