9d4e7043418468043002c241258c18f5c3715f3c
Steve Wolfe <swolfe100@gmail.com>
2025-11-10 16:34:37 -0500

    first commit whisperx

---
+++ create-repo.sh
@@ -0,0 +1,9 @@
+echo "# Whisperx" >> README.md
+git init
+git add README.md
+git status
+git commit -m "first commit"
+git branch -M main
+#git remote add origin https://github.com/steve100/Whisperx.git
+git remote add origin git@github.com:steve100/whisperx.git
+git push -u origin main
